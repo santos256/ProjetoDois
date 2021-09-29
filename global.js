1f31c48c942e4e19020dbbea21d4b353d382aaf8
santos256 <52789790+santos256@users.noreply.github.com>
2021-09-29 09:41:03 -0300

    20210929_02

--- global.js
+++ global.js
@@ -1,10 +1,16 @@
+/***** Configurações do aplicativo *****/
+
+// Nome do site
+var siteName = 'ProjetoDois';
+
+// Quando documento estiver pronto, executa JavaScript
 $(document).ready(runApp);
 
 /** Aplicativo principal */
 function runApp() {
 
     // Carrega página inicial
-    loadPage('home', 'Página inicial');
+    loadPage('home');
 
     // Monitora cliques nos links
     $(document).on('click', 'a', routerLink);
@@ -13,28 +19,68 @@ function runApp() {
 
 // Carrega uma página completa
 function loadPage(pagePath, pageName = '') {
-    const path = `/pages/${pagePath}/${pagePath}`;
-    $('#pageCSS').load(`${path}.css`, () => {
-        $('#pageHTML').load(`${path}.html`, () => {
-            $.getScript(`${path}.js`, () => {
-                $('title').text(`ProjetoDois - ${pageName}`);
-            });
+
+    // Variáveis da função
+    var path, page = {};
+
+    // Divide a rota em partes
+    var parts = pagePath.split('/');
+
+    // Gera rota para HTML
+    if (parts.length == 1)
+        page.html = `/pages/${parts[0]}/${parts[0]}.html`;
+    else
+        page.html = `/pages/${parts[0]}/${parts[0]}.html?${parts[1]}`;
+
+    // Gera rotas para CSS e JS
+    page.css = `/pages/${parts[0]}/${parts[0]}.css`;
+    page.js = `/pages/${parts[0]}/${parts[0]}.js`;
+
+    console.log(page);
+
+    // Carrega componentes da página
+    $('#pageCSS').load(page.css, () => {
+        $('#pageHTML').load(page.html, () => {
+            $.getScript(page.js);
         });
     });
 }
 
 // Roteamento de links
 function routerLink() {
+
+    // Obtém atributos do link
     var href = $(this).attr('href');
     var target = $(this).attr('target');
-    console.log(href, target);
 
     // Resolver âncoras
-
-    if (target == '_blank') {
-        window.open(href);
-    } else {
-
+    if (href.substr(0, 1) == '#') {
+        return true;
     }
+
+    // Resolver links externos
+    if (target == '_blank' || href.substr(0, 7) == 'http://' || href.substr(0, 8) == 'https://') {
+        return true;
+    }
+
+    // Resolver links internos (rotas)
+    loadPage(href);
+
+    // Sai sem fazer nada
     return false;
+}
+
+// Processa título da página <title>
+function setTitle(pageTitle) {
+
+    // Inicializa variável
+    var title;
+
+    if (pageTitle == '')
+        title = siteName;
+    else
+        title = `${siteName} .:. ${pageTitle}`;
+
+    // Escreve na tag <title>
+    $('title').text(title);
 }
---
+++ pages/contacts/contacts.js
@@ -0,0 +1,9 @@
+// Título da página
+var pageTitle = 'Faça Contato';
+
+$(document).ready(runPage);
+
+function runPage() {
+    console.log('Executando a Contacts');
+    
+}
