7fad1303e44c20407889e2d2c35218c8e7aca980
santos256 <52789790+santos256@users.noreply.github.com>
2021-09-29 11:44:45 -0300

    listagem de artigos

--- global.js
+++ global.js
@@ -3,6 +3,23 @@
 // Nome do site
 var siteName = 'ProjetoDois';
 
+// Conexão com o FireBase
+const firebaseConfig = {
+    apiKey: "AIzaSyC8gi_wDO-RGK-txhy74tRXAVyc73jOfI4",
+    authDomain: "projetoum-d38b6.firebaseapp.com",
+    projectId: "projetoum-d38b6",
+    storageBucket: "projetoum-d38b6.appspot.com",
+    messagingSenderId: "138745288607",
+    appId: "1:138745288607:web:b228ebff02ef0835018faa"
+};
+
+// Initializa Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+
+// Armazena dados do usuário logado
+var user;
+
 // Quando documento estiver pronto, executa JavaScript
 $(document).ready(runApp);
 
--- pages/home/home.js
+++ pages/home/home.js
@@ -1,5 +1,6 @@
-// Define o título da página
+// Inicializa variáveis
 var pageTitle = '';
+var artList = '<h2>Artigos Recentes</h2>';
 
 $(document).ready(runPage);
 
@@ -8,6 +9,34 @@ function runPage() {
     // Altera o título da página
     setTitle(pageTitle);
 
-    console.log('Executando a home');
+    // Obtendo todos os artigos do banco de dados
+    db.collection("articles").where('status', '==', 'ativo').orderBy('date', 'desc').onSnapshot((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+                // console.log(`${doc.id} => ${doc.data().img}`);
 
+                // Armazena artigo em 'art'
+                art = doc.data();
+
+                artList += `
+
+    <div class="card" data-route="view/${doc.id}">
+        <div class="card-img" style="background-image: url('${art.img}')"></div>
+            <div class="card-content">
+            <h3>${art.title}</h3>
+            ${art.intro}
+    </div>
+</div>                
+
+            `;
+
+            });
+
+            $('#artList').html(artList);
+        });
+
+}
+
+// Abre o artigo completo
+function viewArticle() {
+    loadPage($(this).attr('data-route'));
 }
